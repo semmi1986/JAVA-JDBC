d850431badbabb91c4c913b9ca5661c8ef2139c9
Nikita Lakhov <kesarbin1@yandex.ru>
2023-06-06 23:35:33 +0300

    tests is working, main and util on queue

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import org.postgresql.Driver;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "admin";
+        try (var connection = DriverManager.getConnection(url, username, password);
+            var statement = connection.createStatement()){
+
+        }
     }
 }
