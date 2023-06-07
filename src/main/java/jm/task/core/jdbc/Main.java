722092c8ed25bbef91871b3bbb3bb626ba19b396
Nikita Lakhov <kesarbin1@yandex.ru>
2023-06-07 21:35:06 +0300

    jdbc done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,27 @@
 package jm.task.core.jdbc;
 
-import org.postgresql.Driver;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
+
 
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
     public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        String password = "admin";
-        try (var connection = DriverManager.getConnection(url, username, password);
-            var statement = connection.createStatement()){
-
+        userService.createUsersTable();
+        userService.saveUser("1", "1", (byte) 1);
+        userService.saveUser("2", "2", (byte) 2);
+        userService.saveUser("3", "3", (byte) 3);
+        userService.saveUser("4", "4", (byte) 4);
+        List<User> users = userService.getAllUsers();
+        for (User user : users){
+            System.out.println(user.getName()+" "+user.getLastName()+" "+user.getAge());
         }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,136 +1,34 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import org.postgresql.util.PSQLException;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDao userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        String password = "admin";
-        String sql = """
-                CREATE TABLE users (
-                    id SERIAL PRIMARY KEY ,
-                    name TEXT,
-                    lastname TEXT,
-                    age INT                    
-                )
-                """;
-        try (var connection = DriverManager.getConnection(url, username, password);
-             var preparedStatement = connection.prepareStatement(sql)){
-            var result = preparedStatement.executeQuery();
-            System.out.println(result);
-            System.out.println("cre");
-        }
-        catch (SQLException e){
-            System.out.println("cre");
-        }
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        String password = "admin";
-        String sql = """
-                DROP TABLE users
-                """;
-        try (var connection = DriverManager.getConnection(url, username, password);
-             var preparedStatement = connection.prepareStatement(sql)){
-            var result = preparedStatement.executeQuery();
-            System.out.println(result);
-            System.out.println("del");
-        }
-        catch (SQLException e){
-            System.out.println("del");
-        }
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        String password = "admin";
-        String sql = """
-                INSERT INTO users (name, lastname, age)
-                VALUES (? , ?, ?)
-                """;
-        try (var connection = DriverManager.getConnection(url, username, password);
-             var preparedStatement = connection.prepareStatement(sql)){
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            var result = preparedStatement.executeQuery();
-            System.out.println(result);
-            System.out.println("saved");
-        }
-        catch (SQLException e){
-            System.out.println("saved");
-        }
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        String password = "admin";
-        String sql = """
-                DELETE FROM users WHERE id = ?
-                """;
-        try (var connection = DriverManager.getConnection(url, username, password);
-             var preparedStatement = connection.prepareStatement(sql)){
-            preparedStatement.setLong(1, id);
-            var result = preparedStatement.executeQuery();
-            System.out.println("remId");
-        }
-        catch (SQLException e){
-            System.out.println("remId");
-        }
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        String password = "admin";
-        String sql = """
-                SELECT * FROM users
-                """;
-        List <User> ret = new ArrayList<>();
-        try (var connection = DriverManager.getConnection(url, username, password);
-             var preparedStatement = connection.prepareStatement(sql)){
-            var result = preparedStatement.executeQuery();
-            System.out.println("getted");
-            while (result.next()){
-                String name = result.getString("name");
-                String lastname = result.getString("lastname");
-                Byte age = result.getByte("age");
-                User us = new User(name, lastname,age);
-                ret.add(us);
-            }
-        }
-        catch (SQLException e){
-            System.out.println("del");
-        }
-        return ret;
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        String password = "admin";
-        String sql = """
-                TRUNCATE TABLE users
-                """;
-        try (var connection = DriverManager.getConnection(url, username, password);
-             var preparedStatement = connection.prepareStatement(sql)){
-            var result = preparedStatement.executeQuery();
-            System.out.println(result);
-            System.out.println("trun");
-        }
-        catch (SQLException e){
-            System.out.println("trun");
-        }
+        userDao.cleanUsersTable();
     }
 }
