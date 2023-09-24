ea64bdd22022955e4bf4c2b786404069c4c64347
antonksanf <antonksanf@gmail.com>
2023-09-24 22:00:46 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,7 +2,15 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.closeConnection;
+import static jm.task.core.jdbc.util.Util.openConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
@@ -10,26 +18,85 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String sqlQuery = "CREATE TABLE Users (id SERIAL PRIMARY KEY, name varchar(255), lastname varchar(255), age smallint);";
+        try {
+            Statement statement = openConnection().createStatement();
+            statement.executeUpdate(sqlQuery);
+            closeConnection();
+        } catch (SQLException e) {
+            System.out.println("Таблица уже существует");
+        }
     }
 
     public void dropUsersTable() {
-
+        String sqlQuery = "DROP TABLE IF EXISTS Users";
+        try {
+            Statement statement = openConnection().createStatement();
+            statement.executeUpdate(sqlQuery);
+            System.out.println("Таблица удалена");
+            closeConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sqlQuery = "INSERT INTO Users (name, lastname, age) VALUES (?,?,?)";
+        try {
+            PreparedStatement preparedStatement = openConnection().prepareStatement(sqlQuery);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.printf("User с именем %s добавлен в базу\n", name);
+            closeConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sqlQuery = "delete from Users where id=?";
+        try {
+            PreparedStatement preparedStatement = openConnection().prepareStatement(sqlQuery);
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            closeConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> listUser = new ArrayList<>();
+        try {
+            Statement statement = openConnection().createStatement();
+            String sqlQuery = "SELECT * FROM Users";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastname"));
+                user.setAge(resultSet.getByte("age"));
+                listUser.add(user);
+                closeConnection();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return listUser;
     }
 
     public void cleanUsersTable() {
-
+        String sqlQuery = "TRUNCATE TABLE Users";
+        try {
+            PreparedStatement statement = openConnection().prepareStatement(sqlQuery);
+            statement.executeUpdate();
+            System.out.println("База данных очищена");
+            closeConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String dbDriver = "org.postgresql.Driver";
+    private static final String dbURL = "jdbc:postgresql://localhost:5432/local_db";
+    private static final String dbUser = "postgres";
+    private static final String dbPass = "1234";
+    private static Connection connection;
+
+    public static Connection openConnection() {
+        try {
+            Class.forName(dbDriver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(dbURL, dbUser, dbPass);
+            //System.out.println("Get connection");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+            // System.out.println("Connection closed");
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
 }
