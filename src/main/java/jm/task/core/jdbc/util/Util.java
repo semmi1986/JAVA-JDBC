68688a46f4848d610d469b3b5b7f1e9665c935d1
Samosiuk Siamion <samasiuk1986@gmail.com>
2024-09-11 17:20:16 +0300

    add DB connection settings

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/itMentor";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "admin";
+
+    private static Connection connection(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
